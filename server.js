74af93afa828fe45fab9af534b844f75d6894866
makrandr1999 <35958423+makrandr1999@users.noreply.github.com>
2018-02-24 09:59:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 var pool= new Pool(config);
-app.get('/test-db', function(req,res)){
+app.get('/test-db', function(req,res){
   pool.query("SELECT * FROM test", function(err,result){
       if(err)
          return res.status(500).send(err.toString());
