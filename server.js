8f4e18d08c429314ab4d60cd588eaec48f3210c5
Makrand R <35958423+makrandr1999@users.noreply.github.com>
2018-03-09 19:04:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ app.get('/articles/:articleName', function(req,res){
           else
              {
                 var articleData=result.rows[0];
-                res.send(articleData);
+                res.send(createTemplate(articleData));
              }
       
               
