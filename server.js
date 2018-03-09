9dcf08c01bc87a59a198ae6c6f24ea2913775b22
Makrand R <35958423+makrandr1999@users.noreply.github.com>
2018-03-09 18:35:13 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -28,17 +28,22 @@ function createTemplate(data){
 app.get('/articles/:articleName', function(req,res){
   // var articleName=;
    //var articleData=
-   pool.query("SELECT * FROM article WHERE title = '" +req.params.articleName + "'", function(err,result){
+   pool.query("SELECT * FROM article WHERE title = '"+req.params.articleName+"'", function(err,result){
       if(err){
           res.status(500).send(err.toString());
       } 
       else {
-          if(result.rows.length === 0)
+          if(result.rows.length === 0){
             res.status(404).send('Article Not Found.');
+          }
           else
+             {
             var articleData=result.rows[0];
             req.send(createTemplate(articleData));
-      }
+              }
+      
+              
+          }
    });
    
 });
