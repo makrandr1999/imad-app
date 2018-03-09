5dc9cf2897483a888ffae4f0ffa982f24a0a323d
Makrand R <35958423+makrandr1999@users.noreply.github.com>
2018-03-09 18:59:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ function createTemplate(data){
     var heading =data.heading;
     var date =data.date;
     var content =data.content;
-    var htmlTemplate = '<html><head><title> ${title}</title><meta name="viewport" content="width=device-width" initial-scale="1"/><link href="/ui/style.css" rel="stylesheet" /></head><body><div class="container"><div><a href="/">Home</a></div><hr/><h3> ${heading}</h3><div>${date}</div><div>${content}</div></div></body></html>' ;
+    var htmlTemplate = '<html><head><title>${title}</title><meta name="viewport" content="width=device-width" initial-scale="1"/><link href="/ui/style.css" rel="stylesheet" /></head><body><div class="container"><div><a href="/">Home</a></div><hr/><h3> ${heading}</h3><div>${date}</div><div>${content}</div></div></body></html>' ;
     return htmlTemplate;
 }
 app.get('/articles/:articleName', function(req,res){
@@ -39,7 +39,7 @@ app.get('/articles/:articleName', function(req,res){
           else
              {
                 var articleData=result.rows[0];
-                res.send(createTemplate(articleData));
+                res.send(articleData);
              }
       
               
