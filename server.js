b8fea73fc858f4b8e137a7dc8e949fb165ebd99c
Makrand R <35958423+makrandr1999@users.noreply.github.com>
2018-03-09 18:04:37 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -30,10 +30,10 @@ app.get('/articles/:articleName', function(req,res){
             res.status(404).send('Article Not Found.');
           else
             var articleData=result.rows[0];
-            
+            req.send(articleData);
       }
    });
-   req.send(articleData);
+   
 });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
