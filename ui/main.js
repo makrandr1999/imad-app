d97b9b38fdd1e5d2b4ec973379f9ac317ef030a6
Makrand R <35958423+makrandr1999@users.noreply.github.com>
2018-03-12 00:16:21 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,19 @@
 console.log('Loaded!');
+var submit = document.getElementById('submit-btn');
+submit.onclick = function (){
+    var request = new XMLHttprequest();
+    
+    request.onreadystatechange= function (){
+          if(request.readystate=== XMLhttprequest.DONE){
+              if(request.status === 200){
+                  console.log('user logged in');
+              }
+          }
+    };
+};
+var username=document.getElementById('username').value;
+var password=document.getElementById('password').value;
+console.log(username);
+console.log(password);
+request.open('POST','http://makrandr.imad.hasura-app.io/login',true);
+request.send(JSON.stringify({username:username , password:password}));
