73c13ab24150c9f3003d674ee134cc95d5c94fae
Makrand R <35958423+makrandr1999@users.noreply.github.com>
2018-03-12 11:56:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -75,7 +75,7 @@ app.post('/login',function(req,res){
                 res.status(403).send('username/password incorrect');
               }
               else{
-                  var dBstring= result.rows[0].password;
+                  var dbString= result.rows[0].password;
                   var salt=dbString.split('$')[2];
                   var hashedPassword = hash(password,salt);
                    if(hashedPassword === dbString){
